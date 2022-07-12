9f1d164bdc69aa8ba600dd40e2524772500ac7ef
divsergey <76725840+divsergey@users.noreply.github.com>
2022-07-12 12:14:49 +0300

    Create swiper.js

---
+++ Project/js/swiper.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded",function() {
+
+var mySwiper = new Swiper(".js-swiper-container", {
+    direction: "horizontal",
+    loop: true,
+  pagination: {
+        el: ".js-swiper-pagination",
+        clickable: "true",
+      },
+  })
+  
+})
