e823e996750ac8df32afb7f4f937ef08e6483ab4
divsergey <76725840+divsergey@users.noreply.github.com>
2022-07-12 12:33:03 +0300

    Create gsap-burger.js

---
+++ Animation/src/js/gsap-burger.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded",function() {
+
+
+    setTimeout (() => {
+        gsap.from(".js__menu", {
+            duration:1,
+             opacity:0,
+             y:50,
+             delay:1
+        });
+
+        gsap.from(".js__main__list", {
+            duration:1,
+             opacity:0,
+             y:100,
+            delay:2.5
+        });
+
+        gsap.from(".js__social-list", {
+            duration:1,
+             opacity:0,
+             y:100,
+            delay:3
+        });
+
+        gsap.from(".js__menu__extra", {
+            duration:1,
+             opacity:0,
+             y:100,
+            delay:3
+        });
+
+    
+
+    })
+
+})
